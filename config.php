fcab0551d4274b9014200974634736be53bc0ca4
Slavi <slavi@orbisius.com>
2019-03-14 19:03:23 +0200

    rm some comments

--- config.php
+++ config.php
@@ -5,6 +5,4 @@ define('ORBISIUS_DO_INTEGRATION_BASE_DIR', __DIR__);
 // Generate one from https://cloud.digitalocean.com/account/api/tokens
 define('ORBISIUS_DO_INTEGRATION_API_TOKEN', '==== INSERT TOKEN HERE ====');
 
-// list dns records
-//$res_arr = $digital_ocean_api->call('/domains/wpsandbox.org
 require_once ORBISIUS_DO_INTEGRATION_BASE_DIR . '/orbisius-integration-digitalocean-api.class.php';
