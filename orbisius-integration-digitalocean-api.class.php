6970aea2a989e2af58c7383149c370f97f35111a
Slavi Marinov <admin@orbisius.com>
2019-08-17 21:12:07 +0300

    integration checks for ENV: ORBISIUS_DO_INTEGRATION_API_TOKEN and uses it as a token. fixed how we check for a valid recordtype

--- orbisius-integration-digitalocean-api.class.php
+++ orbisius-integration-digitalocean-api.class.php
@@ -26,170 +26,204 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 class Orbisius_Integration_DigitalOcean_API {
 	private $ver = '1.0.1';
+
+	/**
+	 * @desc https://developers.digitalocean.com/documentation/v2/#domain-records
+	 */
 	const REC_TYPE_A = 'A';
 	const REC_TYPE_MX = 'MX';
+	const REC_TYPE_NS = 'NS';
+	const REC_TYPE_CAA = 'CAA';
 	const REC_TYPE_TXT = 'TXT';
+	const REC_TYPE_SRV = 'SRV';
+	const REC_TYPE_AAAA = 'AAAA';
 	const REC_TYPE_CNAME = 'CNAME';
 
-    private $api_token = '';
-    private $api_base_url = 'https://api.digitalocean.com/v2';
-    private $params = [];
+	private $api_token = '';
+	private $api_base_url = 'https://api.digitalocean.com/v2';
+	private $params = [];
 
-    public function __construct($params = []) {
-        $this->init($params);
-    }
+	public function __construct($params = []) {
+		$this->init($params);
+	}
 
-    /**
-     * @param array $params
-     * @throws Exception
-     */
-    public function init($params = []) {
-        if (empty($params['token']) || !preg_match('#^[a-z\d]+$#si', $params['token'])) {
-            throw new Exception("API Token was not passed or is invalid.");
-        }
+	/**
+	 * @param array $params
+	 * @throws Exception
+	 */
+	public function init($params = []) {
+		$env_api_token = getenv('ORBISIUS_DO_INTEGRATION_API_TOKEN');
 
-        $this->api_token = $params['token'];
-        $this->params = $params;
-    }
+		if (!empty($env_api_token)) {
+			// cool
+		} elseif (empty($params['token']) || !preg_match('#^[a-z\d]+$#si', $params['token'])) {
+			throw new Exception("API Token was not passed or is invalid.");
+		} else {
+			$env_api_token = $params['token'];
+		}
 
-    const METHOD_GET = 'GET';
-    const METHOD_PUT = 'PUT';
-    const METHOD_HEAD = 'HEAD';
-    const METHOD_POST = 'POST';
-    const METHOD_DELETE = 'DELETE';
+		$this->params = $params;
+		$this->api_token = $env_api_token;
+	}
 
-    /**
-     * Calls DigitalOcean API
-     *
-     * @param string $api_segment
-     * @param array $data
-     * @param string $api_method
-     * @param array $res_arr
-     */
-    function call($api_segment, $req_params = [], $api_method = '') {
-        $res_arr = [
-            'msg' => '',
-            'status' => 0,
-            'data' => [],
-        ];
+	const METHOD_GET = 'GET';
+	const METHOD_PUT = 'PUT';
+	const METHOD_HEAD = 'HEAD';
+	const METHOD_POST = 'POST';
+	const METHOD_DELETE = 'DELETE';
 
-        $timeout = 15;
-        $connect_timeout = 5;
-        $api_token = $this->api_token;
+	/**
+	 * Calls DigitalOcean API
+	 *
+	 * @param string $api_segment
+	 * @param array $data
+	 * @param string $api_method
+	 * @param array $res_arr
+	 */
+	function call($api_segment, $req_params = [], $api_method = '') {
+		$res_arr = [
+			'msg' => '',
+			'status' => 0,
+			'data' => [],
+		];
 
-        if (empty($api_method)) {
-            // /records/123 get record meta info
-            if (preg_match('#/\d+$#si', $api_method)) {
-                if (empty($req_params)) { // no req data -> so need data
-                    $api_method = self::METHOD_HEAD;
-                } else { // updating existing record
-                    $api_method = self::METHOD_PUT;
-                }
-            } else {
-                $api_method = self::METHOD_GET;
-            }
-        } else {
-            $api_method = strtoupper($api_method);
-        }
+		$timeout = 15;
+		$connect_timeout = 5;
+		$api_token = $this->api_token;
 
-        $ch = curl_init();
+		if (empty($api_method)) {
+			// /records/123 get record meta info
+			if (preg_match('#/\d+$#si', $api_method)) {
+				if (empty($req_params)) { // no req data -> so need data
+					$api_method = self::METHOD_HEAD;
+				} else { // updating existing record
+					$api_method = self::METHOD_PUT;
+				}
+			} else {
+				$api_method = self::METHOD_GET;
+			}
+		} else {
+			$api_method = strtoupper($api_method);
+		}
 
-        curl_setopt($ch, CURLOPT_URL, $this->api_base_url . $api_segment);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $api_method);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-        curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
+		$ch = curl_init();
 
-        // Good practice to let people know who's accessing their servers.
-        // See https://en.wikipedia.org/wiki/User_agent
-        $email = empty($this->params['email']) ? 'n/a' : $this->params['email'];
-        curl_setopt($ch, CURLOPT_USERAGENT, sprintf('Orbisius_DigitalOcean_Integration/1.0 (email: %s)', $email));
+		curl_setopt($ch, CURLOPT_URL, $this->api_base_url . $api_segment);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $api_method);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+		curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
 
-        // Set timeouts
-        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connect_timeout);
+		// Good practice to let people know who's accessing their servers.
+		// See https://en.wikipedia.org/wiki/User_agent
+		$email = empty($this->params['email']) ? 'n/a' : $this->params['email'];
+		curl_setopt($ch, CURLOPT_USERAGENT, sprintf('Orbisius_DigitalOcean_Integration/1.0 (email: %s)', $email));
 
-        // Does the user want to turn off SSL certificate verification?
-        // verify_ssl => 0,
-        if (isset($this->params['verify_ssl']) && empty($this->params['verify_ssl'])) {
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        }
+		// Set timeouts
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connect_timeout);
 
-        if (!empty($req_params)) {
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req_params));
-        }
+		// Does the user want to turn off SSL certificate verification?
+		// verify_ssl => 0,
+		if (isset($this->params['verify_ssl']) && empty($this->params['verify_ssl'])) {
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		}
 
-        //Set your auth headers
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Accept: application/json',
-            'Content-Type: application/json',
-            "Cache-Control: no-cache",
-            'Authorization: Bearer ' . $api_token,
-        ));
+		if (!empty($req_params)) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req_params));
+		}
 
-        $buffer = curl_exec($ch);
+		//Set your auth headers
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Accept: application/json',
+			'Content-Type: application/json',
+			"Cache-Control: no-cache",
+			'Authorization: Bearer ' . $api_token,
+		));
 
-        // 200 is code from a regular request, 201 when a record is created, 204 is when it's deleted
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $res_arr['status'] = !empty($buffer) && in_array( $http_code, [ 200, 201, 204, ] );
+		$buffer = curl_exec($ch);
 
-        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $header = substr($buffer, 0, $header_size);
-        $header = trim($header);
-        $body = substr($buffer, $header_size);
-        $body = trim($body);
+		// 200 is code from a regular request, 201 when a record is created, 204 is when it's deleted
+		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$res_arr['status'] = !empty($buffer) && in_array( $http_code, [ 200, 201, 204, ] );
 
-        $res_arr['data']['raw_headers'] = trim($header);
-        $res_arr['data'] = empty($body) ? [] : json_decode($body, true);
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($buffer, 0, $header_size);
+		$header = trim($header);
+		$body = substr($buffer, $header_size);
+		$body = trim($body);
 
-        if (!empty($this->params['debug'])) { //  || empty($res_arr['status'])
-            $info = curl_getinfo($ch);
-            $res_arr['data']['debug'] = $info;
-        }
+		$res_arr['data']['raw_headers'] = trim($header);
+		$res_arr['data'] = empty($body) ? [] : json_decode($body, true);
 
-        curl_close($ch);
+		if (!empty($this->params['debug'])) { //  || empty($res_arr['status'])
+			$info = curl_getinfo($ch);
+			$res_arr['data']['debug'] = $info;
+		}
 
-        return $res_arr;
-    }
+		curl_close($ch);
 
-    /**
-     * @param $api_segment
-     * @param array $data
-     * @return array
-     */
-    public function create($api_segment, array $data ) {
-        $res_arr = $this->call($api_segment, $data, Orbisius_Integration_DigitalOcean_API::METHOD_POST);
-        return $res_arr;
-    }
+		return $res_arr;
+	}
 
-    /**
-     * @param $api_segment
-     * @return array
-     */
-    public function get($api_segment) {
-        $res_arr = $this->call($api_segment);
-        return $res_arr;
-    }
+	/**
+	 * @param $api_segment
+	 * @param array $data
+	 * @return array
+	 */
+	public function create($api_segment, array $data ) {
+		$res_arr = $this->call($api_segment, $data, Orbisius_Integration_DigitalOcean_API::METHOD_POST);
+		return $res_arr;
+	}
 
-    /**
-     * @param $api_segment
-     * @return array
-     */
-    public function delete($api_segment) {
-        $res_arr = $this->call($api_segment, [], Orbisius_Integration_DigitalOcean_API::METHOD_DELETE);
-        return $res_arr;
-    }
+	/**
+	 * @param $api_segment
+	 * @return array
+	 */
+	public function get($api_segment) {
+		$res_arr = $this->call($api_segment);
+		return $res_arr;
+	}
 
-    /**
-     * @param $api_segment
-     * @param array $data
-     * @return array
-     */
-    public function update($api_segment, array $data ) {
-        $res_arr = $this->call($api_segment, $data, Orbisius_Integration_DigitalOcean_API::METHOD_PUT);
-        return $res_arr;
-    }
+	/**
+	 * @param $api_segment
+	 * @return array
+	 */
+	public function delete($api_segment) {
+		$res_arr = $this->call($api_segment, [], Orbisius_Integration_DigitalOcean_API::METHOD_DELETE);
+		return $res_arr;
+	}
+
+	/**
+	 * @param $api_segment
+	 * @param array $data
+	 * @return array
+	 */
+	public function update($api_segment, array $data ) {
+		$res_arr = $this->call($api_segment, $data, Orbisius_Integration_DigitalOcean_API::METHOD_PUT);
+		return $res_arr;
+	}
+
+	/**
+	 * Checks if the type is valid.
+	 * @param string $record_type
+	 * @return bool
+	 */
+	public function isValidRecordType($record_type) {
+		$record_types = [
+			self::REC_TYPE_A,
+			self::REC_TYPE_TXT,
+			self::REC_TYPE_MX,
+			self::REC_TYPE_NS,
+			self::REC_TYPE_AAAA,
+			self::REC_TYPE_CAA,
+			self::REC_TYPE_SRV,
+			self::REC_TYPE_CNAME,
+		];
+
+		return in_array($record_type, $record_types);
+	}
 }
