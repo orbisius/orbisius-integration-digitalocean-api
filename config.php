4ae6c16fed41a696b20392556cd3f8fbb59e67c8
Slavi Marinov <admin@orbisius.com>
2019-08-17 19:37:14 +0300

    checking for ENV: ORBISIUS_DO_INTEGRATION_API_TOKEN and use it if defined.

--- config.php
+++ config.php
@@ -3,6 +3,10 @@
 define('ORBISIUS_DO_INTEGRATION_BASE_DIR', __DIR__);
 
 // Generate one from https://cloud.digitalocean.com/account/api/tokens
-define('ORBISIUS_DO_INTEGRATION_API_TOKEN', '==== INSERT TOKEN HERE ====');
+if (getenv('ORBISIUS_DO_INTEGRATION_API_TOKEN')) {
+	define('ORBISIUS_DO_INTEGRATION_API_TOKEN', getenv('ORBISIUS_DO_INTEGRATION_API_TOKEN'));
+} else {
+	define('ORBISIUS_DO_INTEGRATION_API_TOKEN', '');
+}
 
 require_once ORBISIUS_DO_INTEGRATION_BASE_DIR . '/orbisius-integration-digitalocean-api.class.php';
