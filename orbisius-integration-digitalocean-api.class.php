39bb9a04f587b3a7d1456e5631fc04f6183aef69
Slavi Marinov <admin@orbisius.com>
2019-08-19 03:12:25 +0300

    made lots of improvements in the API. The tool can read api token from file

--- orbisius-integration-digitalocean-api.class.php
+++ orbisius-integration-digitalocean-api.class.php
@@ -25,7 +25,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 class Orbisius_Integration_DigitalOcean_API {
-	private $ver = '1.0.1';
+	private $ver = '1.0.2';
 
 	/**
 	 * @desc https://developers.digitalocean.com/documentation/v2/#domain-records
@@ -40,6 +40,22 @@ class Orbisius_Integration_DigitalOcean_API {
 	const REC_TYPE_CNAME = 'CNAME';
 
 	private $api_token = '';
+	private $verify_ssl = true;
+
+	/**
+	 * @return bool
+	 */
+	public function getVerifySsl() {
+		return isset($this->verify_ssl) && !empty($this->verify_ssl);
+	}
+
+	/**
+	 * @param bool $verify_ssl
+	 */
+	public function setVerifySsl($verify_ssl) {
+		$this->verify_ssl = $verify_ssl;
+	}
+
 	private $api_base_url = 'https://api.digitalocean.com/v2';
 	private $params = [];
 
@@ -52,18 +68,66 @@ class Orbisius_Integration_DigitalOcean_API {
 	 * @throws Exception
 	 */
 	public function init($params = []) {
-		$env_api_token = getenv('ORBISIUS_DO_INTEGRATION_API_TOKEN');
+		$api_token = getenv('ORBISIUS_DO_INTEGRATION_API_TOKEN');
 
-		if (!empty($env_api_token)) {
+		if (!empty($api_token)) {
 			// cool
-		} elseif (empty($params['token']) || !preg_match('#^[a-z\d]+$#si', $params['token'])) {
-			throw new Exception("API Token was not passed or is invalid.");
-		} else {
-			$env_api_token = $params['token'];
+		} elseif (!empty($params['token'])) {
+			$api_token = $params['token'];
+		} elseif (!empty($params['api_key'])) {
+			$api_token = $params['api_key'];
+		} elseif (!empty($params['key'])) {
+			$api_token = $params['key'];
+		}
+
+		if (isset($params['verify_ssl'])) {
+			$this->setVerifySsl($params['verify_ssl']);
 		}
 
 		$this->params = $params;
-		$this->api_token = $env_api_token;
+
+		if (!empty($api_token)) {
+			$this->setApiToken($api_token);
+		}
+	}
+
+	/**
+	 * Loads and sets a token from (ini) file.
+	 * @see https://certbot-dns-digitalocean.readthedocs.io/en/stable/#credentials
+	 * @param string $file
+	 * @return string
+	 */
+	public function loadApiTokenFromFile($file) {
+		if (!is_file($file)) {
+			return false;
+		}
+
+		$buff = file_get_contents($file, LOCK_SH);
+
+		// We'll use this ini later for DNS stuff
+		// # DigitalOcean API credentials used by Certbot
+		//dns_digitalocean_token = 0000111122223333444455556666777788889999aaaabbbbccccddddeeeeffff
+
+		if (preg_match('#(token|api_key|key)\s*=[\s\'\"]*([\w\-]+)#si', $buff, $matches)) {
+			$api_token = $matches[2];
+			$this->setApiToken($api_token);
+		}
+
+		return $this->getApiToken();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getApiToken() {
+		return $this->api_token;
+	}
+
+	/**
+	 * @param string $api_token
+	 */
+	public function setApiToken($api_token) {
+		$this->api_token = $api_token;
 	}
 
 	const METHOD_GET = 'GET';
@@ -81,6 +145,12 @@ class Orbisius_Integration_DigitalOcean_API {
 	 * @param array $res_arr
 	 */
 	function call($api_segment, $req_params = [], $api_method = '') {
+		$api_token = $this->getApiToken();
+
+		if (empty($api_token)) {
+			throw new Exception("API Token was not passed or is invalid.");
+		}
+
 		$res_arr = [
 			'msg' => '',
 			'status' => 0,
@@ -126,14 +196,18 @@ class Orbisius_Integration_DigitalOcean_API {
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connect_timeout);
 
 		// https://stackoverflow.com/questions/731117/error-using-php-curl-with-ssl-certificates
-		$default_ca_cert_file = __DIR__ . '/cacert.pem';
+		$default_ca_cert_file = dirname(__FILE__) . '/cacert.pem';
 
 		// Does the user want to turn off SSL certificate verification?
 		// verify_ssl => 0,
-		if (isset($this->params['verify_ssl']) && empty($this->params['verify_ssl'])) {
+		if (!$this->getVerifySsl()) {
 			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		} elseif (!empty($this->params['ca_cert_file'])) {
+			if (!file_exists($this->params['ca_cert_file'])) {
+				throw new Exception("The passed ca_cert_file file doesn't exist or is not readable.");
+			}
+
 			curl_setopt($ch, CURLOPT_CAINFO, $this->params['ca_cert_file']);
 			curl_setopt($ch, CURLOPT_CAPATH, $this->params['ca_cert_file']);
 		} elseif (file_exists($default_ca_cert_file)) {
@@ -145,7 +219,7 @@ class Orbisius_Integration_DigitalOcean_API {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req_params));
 		}
 
-		//Set your auth headers
+		// Set auth headers
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 			'Accept: application/json',
 			'Content-Type: application/json',
